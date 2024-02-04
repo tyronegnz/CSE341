e51749e5e1ae0db447774da3e6fc15cb2faa070b
tyronegnz <64057457+tyronegnz@users.noreply.github.com>
2024-02-03 20:43:01 -0700

    Add files via upload
    update week 3

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,6 +5,12 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 
-router.get('/:id', contactsController.getSingle); 
+router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,58 +12,12 @@ app
     next();
   })
   .use('/', require('./routes'));
-  
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
-  }
-});
-
-
-
-
-app.get('/contacts', async (req, res) => {
-  try {
-    // Read the contents of the contacts.json file
-    const data = await fs.readFile('contacts.json', 'utf-8');
-
-    // Parse the JSON data
-    const contacts = JSON.parse(data);
-
-    // Send the contacts as a JSON response
-    res.json(contacts);
-  } catch (error) {
-    // Handle errors, e.g., file not found or invalid JSON format
-    console.error('Error reading contacts.json:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-
-// New route to retrieve a single contact by ID
-app.get('/contacts/:id', async (req, res) => {
-  try {
-    const data = await fs.readFile('contacts.json', 'utf-8');
-    const contacts = JSON.parse(data);
-
-    // Find the contact with the specified ID
-    const contactId = req.params.id;
-    const contact = contacts.find(c => c.id === contactId);
-
-    if (!contact) {
-      // If contact is not found, return a 404 Not Found response
-      res.status(404).json({ error: 'Contact not found' });
-    } else {
-      // If contact is found, return it as a JSON response
-      res.json(contact);
-    }
-  } catch (error) {
-    console.error('Error reading contacts.json:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
