2d8fdf52c4952c667d3c57d9cb6d5dceea30ff27
tyronegnz <64057457+tyronegnz@users.noreply.github.com>
2024-02-03 19:15:21 -0700

    update

--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,3 @@
-/*
 const dotenv = require('dotenv');
 dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
@@ -31,45 +30,3 @@ module.exports = {
   initDb,
   getDb,
 };
-*/
-
-const dotenv = require('dotenv');
-dotenv.config();
-const { MongoClient } = require('mongodb');
-
-let _db;
-
-const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
-  }
-
-  const uri = process.env.MONGODB_URI;
-
-  if (!uri) {
-    return callback(new Error('MONGODB_URI is not defined in the environment variables.'));
-  }
-
-  MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((client) => {
-      _db = client.db(); // Access the database from the client
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
-};
-
-const getDb = () => {
-  if (!_db) {
-    throw new Error('Db not initialized');
-  }
-  return _db;
-};
-
-module.exports = {
-  initDb,
-  getDb,
-};
-
